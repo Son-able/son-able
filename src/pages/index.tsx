95365b71ccf9522d75190f1e378e3aaec401378a
Alyssa Kozma <lyssarzrmusic@gmail.com>
2024-02-19 16:31:02 -0800

    Navbar update. Logo (resizes to icon)  on left, avatar bubble on right. Some theming aplied.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
+'use client'
 import Head from "next/head";
 import Script from "next/script"
+import Image from 'next/image'
 
 import feedTab from "../features/tabs/feedTab"
 
@@ -16,7 +18,6 @@ import {
   MenubarItem,
   MenubarMenu,
   MenubarSeparator,
-  MenubarShortcut,
   MenubarTrigger,
 } from "~/components/ui/menubar"
 
@@ -31,18 +32,25 @@ export default function Home() {
         <meta name="description" content="Sonable" />
         <link rel="icon" href="/favicon.ico" />
         <script src="https://cdn.jsdelivr.net/npm/feather-icons/dist/feather.min.js" async />
+
       </Head>
 
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        <Menubar className="items-end objects-contain" dir="rtl" style={{
+        <Menubar className="items-end objects-contain bg-foreground border-none" dir="rtl" style={{
           position: "absolute", 
           top: "0px", 
           width: "100%", 
+          height: "44px",
           borderRadius: "0px", 
           alignContent: "flex-end", 
           alignItems: "flex-end", 
+          placeContent: "end",
           float: "right",
         }}>
+          <img className="md:hidden" src={"./sonable_icon.png"} style={{position: "absolute", top: "2px", left: "0px", height: "37px"}} />
+          <a href="/" className="hidden md:flex">
+            <img src={"./sonable_logo_transparent.png"} style={{position: "absolute", top: "2px", left: "0px", height: "37px"}} />
+          </a>
           <MenubarMenu>
             <MenubarTrigger dir="rtl" className="objects-contain">
               <Avatar style={{marginBottom: "-7px"}}>
@@ -50,23 +58,22 @@ export default function Home() {
                 <AvatarFallback>Settings</AvatarFallback>
               </Avatar>
             </MenubarTrigger>
-            <MenubarTrigger dir="rtl">
-              <Avatar style={{marginBottom: "-7px"}}>
-                <AvatarImage src="./settings.svg" style={{marginTop: "0px"}} />
-                <AvatarFallback>
-                    <i data-feather="settings"></i>
-                </AvatarFallback>
-              </Avatar>
-            </MenubarTrigger>
             <MenubarContent>
               <MenubarItem>
-                View Profile
+                Messages
               </MenubarItem>
               <MenubarItem>
                 Friends
               </MenubarItem>
               <MenubarItem>
-                Messages
+                View Profile
+              </MenubarItem>
+              <MenubarItem>
+                Edit Profile
+              </MenubarItem>
+              <MenubarSeparator />
+              <MenubarItem>
+                Create New Profile
               </MenubarItem>
               <MenubarSeparator />
               <MenubarItem>
@@ -75,14 +82,11 @@ export default function Home() {
               <MenubarItem>
                 Logout
               </MenubarItem>
-              <MenubarItem>Share</MenubarItem>
-              <MenubarSeparator />
-              <MenubarItem>Print</MenubarItem>
             </MenubarContent>
           </MenubarMenu>
         </Menubar>
-        <Tabs defaultValue="feed" className="w-[33%]" style={{position: "absolute", top: "3px", width: "60%", minWidth: "240px"}}>
-          <TabsList className="grid w-full grid-cols-5">
+        <Tabs defaultValue="feed" className="w-[33%] h-[100%] border-border" style={{position: "absolute", top: "3px", width: "60%", minWidth: "240px"}}>
+          <TabsList className="grid w-full grid-cols-5 bg-foreground fg-foreground" style={{stroke: "red"}}>
             <TabsTrigger value="feed">
               <i data-feather="home"></i>
             </TabsTrigger>
@@ -100,7 +104,9 @@ export default function Home() {
             </TabsTrigger>
           </TabsList>
           <TabsContent value="feed">
-            { feedTab() }
+            <div style={{all: "unset"}}>
+              { feedTab() }
+            </div>
           </TabsContent>
           <TabsContent value="events">
             <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
